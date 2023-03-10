b4c153be091ea948324b5f4562d0087a2b19d332
cluffa <alexcluff16@gmail.com>
2023-03-09 22:56:27 -0500

    julia project

---
+++ julia/notebook.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ de2eb780-a3d6-45b6-add2-f0045aefe1b4
+
+
+# ╔═╡ Cell order:
+# ╠═de2eb780-a3d6-45b6-add2-f0045aefe1b4
